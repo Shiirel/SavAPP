b44ac4ae22c8841893c84d855e1518b7aa33bd21
Shiirel <kassilissal@gmail.com>
2026-02-20 16:13:25 +0100

    1er commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomePage } from './pages/home-page/home-page';
+import { LoginPage} from './pages/login-page/login-page';
+import { SubscribePage } from './pages/subscribe-page/subscribe-page';
+import { AccountManagerPage} from './pages/account-manager-page/account-manager-page';
+import { RecipeCalculatorPage } from './pages/recipe-calculator-page/recipe-calculator-page';
+import { RecipeManagerPage } from './pages/recipe-manager-page/recipe-manager-page';
+import { UsersManagerPage } from './pages/users-manager-page/users-manager-page';
+import { IngredientManagerPage} from './pages/ingredient-manager-page/ingredient-manager-page';
+import { AboutPage } from './pages/about-page/about-page';
+import {LegalPage} from './pages/legal-page/legal-page';
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomePage },
+  // Auth
+  { path: 'login', component: LoginPage },
+  { path: 'subscribe', component: SubscribePage},
+  // Compte
+  { path: 'account', component: AccountManagerPage },
+  // Recettes
+  { path: 'recipe-calculator', component: RecipeCalculatorPage },
+  { path: 'recipe-manager', component: RecipeManagerPage},
+  // Admin / gestion
+  { path: 'users-manager', component: UsersManagerPage },
+  { path: 'ingredient-manager', component: IngredientManagerPage },
+  // Divers
+  { path: 'about', component: AboutPage },
+  { path: 'legal', component: LegalPage },
+  // Fallback
+  { path: '**', redirectTo: 'home' },
+];
